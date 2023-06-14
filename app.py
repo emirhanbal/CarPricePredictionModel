ca8a900f30d2fd553cb70173a92bed21c9c2ab9d
Emirhan Bal <58405854+emirhanbal@users.noreply.github.com>
2023-06-14 11:00:16 +0300

    updates

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+from flask import Flask, request, jsonify , render_template
+
+app = Flask(__name__)
+    
 from pymongo import MongoClient
 from dotenv import load_dotenv, find_dotenv
 import os
+import pprint
 load_dotenv(find_dotenv())
 
 #MongoDB için gerekli kütüphaneler
@@ -9,20 +14,20 @@ connection_string = f"mongodb+srv://emirhanbal:{password}@graduation.r68pz0b.mon
 client = MongoClient(connection_string)
 
 #MONGODB ile kodumu ilişkilendirme adımı. burada database'imi bağlıyorum.
-db = client["ilanlar"]
+db = client["ilanlar_arabamcom"]
 collection = db["arac_detay_arabamcom"]
-dataFromDatabase = list(collection.find())
 
 dataFromDatabase = list(collection.find())
 # Convert entire collection to Pandas dataframe
 import pandas as pd
 _df = pd.DataFrame(dataFromDatabase)
-# verilerimin içerisinde 10binden küçük veya 15milyondan büyük fiyatlı olanları kontrol ediyorum.
-_df[(_df.Fiyat<10000) | (_df.Fiyat>15000000) | (_df.Kilometre>1000000)]
+
 # ve bunları verilerim arasından çıkarıyorum
 _df.drop(_df[(_df.Fiyat < 10000) | (_df.Fiyat > 15000000) | (_df.Kilometre>1000000)].index, inplace=True)
+
 # boya değişen olanları 'Var' olarak değiştiriyorum
 _df.Boya_degisen[(_df.Boya_degisen!='Tamamı orjinal') & (_df.Boya_degisen!='Belirtilmemiş')] = 'Var'
+
 columns_to_keep = ['Fiyat', 'Marka', 'Seri', 'Yil', 'Kilometre', 'Vites_Tipi', 'Yakit_Tipi', 'Boya_degisen'] #'Model', 'Seri',
 _df = _df[columns_to_keep]
 
@@ -36,22 +41,25 @@ for column in _df.columns:
         imputer = KNNImputer(n_neighbors=5)
         column_data = _df[column].values.reshape(-1, 1)
         _df[column] = imputer.fit_transform(column_data)
+
+        
 _df['Yil'] = _df['Yil'].astype(int)
 _df['Fiyat'] = _df['Fiyat'].astype(int)
 _df['Kilometre'] = _df['Kilometre'].astype(int)
-import pandas as pd
+
+
+#import pandas as pd
 
 # One-Hot Encoding işlemi
 yakit_Tipi_encoded = pd.get_dummies(_df['Yakit_Tipi'], prefix='Yakit_Tipi')
 vites_Tipi_encoded = pd.get_dummies(_df['Vites_Tipi'], prefix='Vites_Tipi')
 boya_degisen_encoded = pd.get_dummies(_df['Boya_degisen'], prefix='Boya_degisen')
 
-from sklearn.preprocessing import LabelEncoder
+# Yeni sütunları ekleme
+_df = pd.concat([_df, yakit_Tipi_encoded, vites_Tipi_encoded, boya_degisen_encoded], axis=1)
+_df = _df.drop(['Yakit_Tipi', 'Vites_Tipi', 'Boya_degisen'], axis=1)
 
-# Marka sütununu seçme
-Marka = _df['Marka']
-Seri = _df['Seri']
-
+#DENEME
 from sklearn.preprocessing import LabelEncoder
 
 # Marka sütununu seçme
@@ -69,7 +77,7 @@ _df['Marka_Encoded'] = marka_encoded
 # Her bir sayısal değerin karşılık gelen markasını elde etme
 marka_degerleri = label_encoder.classes_
 # Her bir sayısal değerin ve karşılık gelen markanın ekrana yazdırılması
-marka_sozlugu = {sayisal_deger: marka for sayisal_deger, marka in enumerate(marka_degerleri)}
+marka_sozlugu = {marka: sayisal_deger for sayisal_deger, marka in enumerate(marka_degerleri)}
 
 # Seri sütununu dönüştürme
 seri_encoded = label_encoder.fit_transform(Seri)
@@ -78,14 +86,34 @@ _df['Seri_Encoded'] = seri_encoded
 # Her bir sayısal değerin karşılık gelen serisini elde etme
 seri_degerleri = label_encoder.classes_
 # Her bir sayısal değerin ve karşılık gelen serinin ekrana yazdırılması
-seri_sozlugu = {sayisal_deger: seri for sayisal_deger, seri in enumerate(seri_degerleri)}
-
+seri_sozlugu = {seri: sayisal_deger for sayisal_deger, seri in enumerate(seri_degerleri)}
 
 _df.drop(['Seri'],axis=1,inplace=True)
 _df.drop(['Marka'],axis=1,inplace=True)
-# Yeni sütunları ekleme
-_df = pd.concat([_df, yakit_Tipi_encoded, vites_Tipi_encoded, boya_degisen_encoded], axis=1)
-_df = _df.drop(['Yakit_Tipi', 'Vites_Tipi', 'Boya_degisen'], axis=1)
+
+from sklearn.model_selection import train_test_split
+
+# Bağımsız değişkenler (X) ve hedef değişken (y) olarak ayırma
+X = _df.drop('Fiyat', axis=1)  # Hedef sütunu çıkararak bağımsız değişkenleri alıyoruz
+y = _df['Fiyat']  # Hedef sütunu olarak ayarlanmış olan sütunu alıyoruz
+
+# Veri kümesini eğitim ve test kümelerine ayırma
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+#Random Forest Regressor
+from sklearn.ensemble import RandomForestRegressor
+regressor=RandomForestRegressor(n_estimators=200,min_samples_split=2,min_samples_leaf=2,max_features='sqrt', max_depth=80, bootstrap=True)
+regressor.fit(X_train,y_train)
+y_pred_randf=regressor.predict(X_test)
+
+def predict_price(regressor, input_data):
+    # Girdi verilerini modele uygula ve çıktıyı al
+    predicted_price = regressor.predict(input_data)
+    
+    predicted_price = int(predicted_price)
+    
+    # Tahmin edilen fiyatı döndür
+    return predicted_price
 
 yakit_tipi_sozlugu = {
     'Benzin': [1, 0, 0, 0, 0],
@@ -106,38 +134,10 @@ boya_degisen_sozlugu = {
     'Tamamı orjinal': [0, 1, 0],
     'Var': [0, 0, 1]
 }
-
-from sklearn.model_selection import train_test_split
-
-# Bağımsız değişkenler (X) ve hedef değişken (y) olarak ayırma
-X = _df.drop('Fiyat', axis=1)  # Hedef sütunu çıkararak bağımsız değişkenleri alıyoruz
-y = _df['Fiyat']  # Hedef sütunu olarak ayarlanmış olan sütunu alıyoruz
-
-# Veri kümesini eğitim ve test kümelerine ayırma
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Eğitim ve test kümelerinin boyutlarını kontrol etmek
-print("Eğitim kümesi boyutu:", X_train.shape)
-print("Test kümesi boyutu:", X_test.shape)
-
-#Random Forest Regressor
-from sklearn.ensemble import RandomForestRegressor
-regressor=RandomForestRegressor(n_estimators=200,min_samples_split=2,min_samples_leaf=2,max_features='sqrt', max_depth=80, bootstrap=True)
-regressor.fit(X_train,y_train)
-y_pred_randf=regressor.predict(X_test)
-
-from flask import Flask, request, jsonify , render_template
-
-app = Flask(__name__)
-def predict_price(regressor, input_data):
-    # Girdi verilerini modele uygula ve çıktıyı al
-    predicted_price = regressor.predict(input_data)
-
-    # Tahmin edilen fiyatı döndür
-    return predicted_price
 # HTML formunu görüntülemek için GET isteği
 @app.route('/', methods=['POST', 'GET'])
 def predict():
+    
     if request.method == 'POST':
         # Process the form data and predict
         yil = int(request.form['yil'])
@@ -147,8 +147,8 @@ def predict():
         boya_degisen = request.form['boya_degisen']
         marka = request.form['marka']
         seri = request.form['seri']
-        # ... rest of the processing and prediction code...
-        # Prediction part stays the same
+
+        
         yakit_tipi_kod = yakit_tipi_sozlugu.get(yakit_tipi, [0] * len(yakit_tipi_sozlugu))
         vites_tipi_kod = vites_tipi_sozlugu.get(vites_tipi, [0] * len(vites_tipi_sozlugu))
         boya_degisen_kod = boya_degisen_sozlugu.get(boya_degisen, [0] * len(boya_degisen_sozlugu))
@@ -158,9 +158,16 @@ def predict():
         seri_kod = seri_sozlugu.get(seri, -1)
 
         input_data = [[yil, kilometre] + yakit_tipi_kod + vites_tipi_kod + boya_degisen_kod + [marka_kod, seri_kod]]
-
+        
+        predictions = []
+        user_input = collection.find({"Marka" : marka, "Seri": seri}).sort("Fiyat", 1).limit(3) #, "Boya_degisen" : "Belirtilmemiş"
+        for arac in user_input:
+            link = arac['Link']
+            fiyat = arac['Fiyat']
+            predictions.append((link, fiyat))
+            
         predicted_price = predict_price(regressor, input_data)
-        return render_template('index.html', predicted_price=predicted_price)
+        return render_template('index.html', predicted_price=predicted_price , input_data=input_data, predictions=predictions)
     else:
         return render_template('index.html')
     
@@ -173,3 +180,4 @@ def predict():
     # Process your data here
 if __name__ == '__main__':
     app.run()
+
